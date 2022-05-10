aacc15fbd12c79a603b4593f98669f52d89626f9
Macky Baby <raindrops.dark@gmail.com>
2022-05-10 17:34:22 +0800

    Updates

---
+++ precache-manifest.5df4b4cf40a42363c39b2cf43f386c6c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e987fe16fd495f39a151",
+    "url": "/clavs/static/css/main.64e672d4.chunk.css"
+  },
+  {
+    "revision": "e987fe16fd495f39a151",
+    "url": "/clavs/static/js/main.e987fe16.chunk.js"
+  },
+  {
+    "revision": "877ddeaf8032078f6663",
+    "url": "/clavs/static/js/1.877ddeaf.chunk.js"
+  },
+  {
+    "revision": "ec5bd900186b6d69cf36",
+    "url": "/clavs/static/js/2.ec5bd900.chunk.js"
+  },
+  {
+    "revision": "a8a9c0aad4ffbf087e60",
+    "url": "/clavs/static/js/runtime~main.a8a9c0aa.js"
+  },
+  {
+    "revision": "d7eee0c2e62d0af675db76c38affffbd",
+    "url": "/clavs/static/media/Brown Cake.d7eee0c2.jpg"
+  },
+  {
+    "revision": "a9daebab7dab1d6be8e336a10269e491",
+    "url": "/clavs/static/media/Sample Logo 1.a9daebab.png"
+  },
+  {
+    "revision": "f1c3651a3da5e12a672e3b95ec6da5a1",
+    "url": "/clavs/static/media/Cupcakes Brown lq.f1c3651a.jpg"
+  },
+  {
+    "revision": "62b09ae897c83836ba5361f1788fb763",
+    "url": "/clavs/static/media/Cupcakes lq.62b09ae8.jpg"
+  },
+  {
+    "revision": "e49b5c638c928f0d4ca230737982c583",
+    "url": "/clavs/static/media/Cake lq.e49b5c63.jpg"
+  },
+  {
+    "revision": "55bf274eeeb04dd4f249d2cd997dd0fd",
+    "url": "/clavs/static/media/Generic Muffins.55bf274e.jpg"
+  },
+  {
+    "revision": "d3f90ee7760844d22f4cba50f3d05c43",
+    "url": "/clavs/static/media/Cookies.d3f90ee7.jpg"
+  },
+  {
+    "revision": "55fc7d16f37c66646220afbfae27fe58",
+    "url": "/clavs/static/media/pexels-lucie-liz-3256807.55fc7d16.jpg"
+  },
+  {
+    "revision": "77651133eb6edfd8d390ee17ff8ac522",
+    "url": "/clavs/static/media/Pink Muffins.77651133.jpg"
+  },
+  {
+    "revision": "710c0ee1be595b13b95f728d28eaceed",
+    "url": "/clavs/static/media/Bread.710c0ee1.png"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/clavs/static/media/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "718651c5afbbca8153f7c78027f16a85",
+    "url": "/clavs/static/media/TrendSansOne.718651c5.ttf"
+  },
+  {
+    "revision": "6890001898a6fc1b36ddc2ed0195e2d8",
+    "url": "/clavs/static/media/Montserrat-Regular.68900018.ttf"
+  },
+  {
+    "revision": "57a8d90fbe52cdc657be8d679f925066",
+    "url": "/clavs/static/media/Montserrat-Light.57a8d90f.ttf"
+  },
+  {
+    "revision": "2192443373f4e6d6fe159bd11d497c24",
+    "url": "/clavs/static/media/ThirstyScriptExtraBold.21924433.otf"
+  },
+  {
+    "revision": "0c12ee2fd868459a1db633259b2ba50a",
+    "url": "/clavs/static/media/Montserrat-Bold.0c12ee2f.ttf"
+  },
+  {
+    "revision": "8588f6fedb41275ed6501522a7ad8f2a",
+    "url": "/clavs/static/media/MilasianCircaMediumPERSONAL.8588f6fe.ttf"
+  },
+  {
+    "revision": "2fe5fb7f37535452bc6a61d52d61660a",
+    "url": "/clavs/static/media/libre-baskerville.bold.2fe5fb7f.ttf"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/clavs/static/media/CaviarDreams.3670aa49.ttf"
+  },
+  {
+    "revision": "c513283c055aa42238561a31ad9e6737",
+    "url": "/clavs/static/media/ThirstyRoughLight.c513283c.otf"
+  },
+  {
+    "revision": "d6ee8f76acb15b46f0b3ceda33e676ee",
+    "url": "/clavs/static/media/ThirstyRoughRegular.d6ee8f76.otf"
+  },
+  {
+    "revision": "d422ef619139bc6c46141acea5930260",
+    "url": "/clavs/index.html"
+  }
+];
